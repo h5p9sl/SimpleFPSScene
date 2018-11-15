48e289c0d8a0b74534107e11f58d6dedd39e6976
h5p9sl <h5p9sl@users.noreply.github.com>
2018-11-15 16:55:41 -0700

    Basic Project Setup

--- Main.cpp
+++ Main.cpp
@@ -6,14 +6,16 @@ int main() {
 
     Window window("Simple FPS Scene");
 
+    window.Show(true);
+
     while (window.IsOpen()) {
         WndEvent event;
         while (window.PollEvent(event)) {
             if (event == WndEvent::MSG_CLOSE) window.Close();
         }
 
-        
-
+        window.Render();
+        Sleep(1);
     }
 
     return 0;
